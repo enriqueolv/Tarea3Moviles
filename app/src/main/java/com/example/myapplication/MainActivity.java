fa686fbe62599288742af8df9db882b6058a5356
enriqueolv <kevinenriqueoogm@gmail.com>
2020-09-27 00:47:34 -0500

    Tarea terminada. Primera version

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,12 +7,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;   
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     //Componentes visuales
     Button boton_ver_pedido;
     Button boton_agregar;
+    private ArrayList<Taco> arrayTacos;
+    private Taco taco_actual;
+    private EditText editText_carne;
+    private RadioButton radioButton_salsa_pica, radioButton_salsa_no_pica;
+    private CheckBox checkBox_limon, checkBox_cilantro, checkBox_cebolla;
 
 
     @Override
@@ -25,16 +35,24 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void initComponents(){
+        arrayTacos  = new ArrayList<>();
+        editText_carne            = findViewById(R.id.editText_tipo_de_carne);
+        radioButton_salsa_pica    = findViewById(R.id.radioButton_salsa_pica);
+        radioButton_salsa_no_pica = findViewById(R.id.radioButton_salsa_no_pica);
+        checkBox_limon            = findViewById(R.id.checkBox_limon);
+        checkBox_cilantro         = findViewById(R.id.checkBox_cilantro);
+        checkBox_cebolla          = findViewById(R.id.checkBox_cebolla);
+
         //Se inicializa el boton ver pedido para que dirija a la actividad secundaria
         boton_ver_pedido = (Button) findViewById(R.id.boton_ver_pedido);
         boton_ver_pedido.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                Intent intent2 = new Intent(v.getContext(), SecondaryActivity.class);
-                startActivityForResult(intent2, 0);
+                Intent intent = new Intent(v.getContext(), SecondaryActivity.class);
+                intent.putExtra("arrayTacos", arrayTacos);
+                startActivityForResult(intent, 0);
             }
         });
-
 
         //El boton agregar agrega el taco a la lista y muestra un toast para indicar
         //la acción realizada
@@ -42,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
         boton_agregar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                agregar_taco_actual_a_lista();
                 Context context = getApplicationContext();
                 CharSequence text = "Taco agregado :)";
                 int duration = Toast.LENGTH_SHORT;
@@ -49,9 +68,36 @@ public class MainActivity extends AppCompatActivity {
                 toast.show();
             }
         });
-
-
     }
 
-   
+    private void agregar_taco_actual_a_lista(){
+        taco_actual = new Taco();
+        taco_actual.setCarne(editText_carne.getText().toString());
+        taco_actual.setTipo_salsa(0);
+        if(radioButton_salsa_pica.isChecked()){
+            taco_actual.setTipo_salsa(0);
+        }
+        if(radioButton_salsa_no_pica.isChecked()){
+            taco_actual.setTipo_salsa(1);
+        }
+
+        if(checkBox_limon.isChecked()){
+            taco_actual.setLimon(true);
+        }else{
+            taco_actual.setLimon(false);
+        }
+
+        if(checkBox_cilantro.isChecked()){
+            taco_actual.setCilantro(true);
+        }else{
+            taco_actual.setCilantro(false);
+        }
+
+        if(checkBox_cebolla.isChecked()){
+            taco_actual.setCebolla(true);
+        }else{
+            taco_actual.setCebolla(false);
+        }
+        arrayTacos.add(taco_actual);
+    }
 }
--- app/src/main/java/com/example/myapplication/SecondaryActivity.java
+++ app/src/main/java/com/example/myapplication/SecondaryActivity.java
@@ -5,21 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class SecondaryActivity extends AppCompatActivity {
 
-    Button boton_regresar;
+    private Button boton_regresar;
+    private ArrayList<Taco> arrayTacos;
+    private TacoArrayAdapter adapter;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondary);
         initComponents();
+        cargarListaDeTacos();
     }
 
 
     private void initComponents(){
+        listView = (ListView) findViewById(R.id.listView_lista_tacos);
+        arrayTacos = new ArrayList<>();
         //Inicializa el botón de regresar para retornar a la actividad principal
         boton_regresar = (Button) findViewById(R.id.boton_regresar);
         boton_regresar.setOnClickListener(new View.OnClickListener() {
@@ -29,5 +39,16 @@ public class SecondaryActivity extends AppCompatActivity {
                 startActivityForResult(intent, 0);
             }
         });
+    }
+
+
+    private void cargarListaDeTacos(){
+        /*
+        for(int i=0; i<10; i++){
+            arrayTacos.add(new Taco("Pastor", 1, true, true, true));
+        }*/
+        arrayTacos = (ArrayList<Taco>) getIntent().getSerializableExtra("arrayTacos");
+        adapter = new TacoArrayAdapter(arrayTacos, getApplicationContext());
+        listView.setAdapter(adapter);
     }
 }
